6b01458842e37e17c33652c9a623673bf4e9f339
dlyda <dilaydaakpinar34@gmail.com>
2025-03-01 17:45:42 +0300

    Fullmoon
    Tiyatro web sitesi

---
+++ fullmoon/harici.js
@@ -0,0 +1,29 @@
+function resim1(){
+	document.getElementById("kutu1").style.cssText="opacity:0.4";
+	}
+function resim1(){
+	document.getElementById("kutu1").style.cssText="opacity:0.4";
+	document.getElementById("resim").src="img/dresim.jpeg";
+	}
+function resim2(){
+	document.getElementById("kutu2").style.cssText="opacity:0.4";
+	document.getElementById("resim").src="img/oyun33.jpg";
+	}
+function resim3(){
+	document.getElementById("kutu3").style.cssText="opacity:0.4";
+	document.getElementById("resim").src="img/film4.jpg";
+	}
+function resim4(){
+	document.getElementById("kutu4").style.cssText="opacity:0.4";
+	document.getElementById("resim").src="img/film5.jpg";
+	}
+function iptal(){
+	document.getElementById("kutu1").style.cssText="opacity:1";
+	document.getElementById("kutu2").style.cssText="opacity:1";
+	document.getElementById("kutu3").style.cssText="opacity:1";
+	document.getElementById("kutu4").style.cssText="opacity:1";
+	document.getElementById("resim").src="img/dresim.jpeg";
+	}
+
+	
+	
